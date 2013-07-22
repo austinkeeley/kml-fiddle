85b9cd61d355ec35f4938cdb5d337d89288c78ca
Austin Keeley <austin.keeley@gmail.com>
2013-07-21 22:14:25 -0400

    Adding initial Rails files

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+KmlFiddle::Application.config.session_store :cookie_store, key: '_kml-fiddle_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# KmlFiddle::Application.config.session_store :active_record_store
